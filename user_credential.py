bec40f68640d1b7e6d5d1097aadf1947ec568146
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-02-19 15:24:32 +0300

    add class

--- user_credential.py
+++ user_credential.py
@@ -2,9 +2,11 @@ import pyperclip
 import random
 import string
 
-# Global Details
+# Global Variables
 global users_list 
 class User:
 	'''
 	Class to create user account and save their detail
 	'''
+    #Class Details
+    #global users_list
