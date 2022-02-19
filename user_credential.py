5159b2d5d33c48af6dd5d8557f041f2cf819b18d
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-02-19 15:34:29 +0300

    add first name, last name and password

--- user_credential.py
+++ user_credential.py
@@ -6,7 +6,17 @@ import string
 global users_list 
 class User:
 	'''
-	Class to create user account and save their detail
+	Class to create user account and save their details
 	'''
     #Class Details
     #global users_list
+    users_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		Method to define the properties/characteristics for each user details input.
+		'''
+
+		# Details Init
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
