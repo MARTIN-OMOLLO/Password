529d76329a57dc37d024f22596772b564c5795cd
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-02-20 20:31:18 +0300

    account details las part has been added and concluded

--- user_run.py
+++ user_run.py
@@ -70,4 +70,59 @@ def main():
             print("\n \n")
             # for user in display_users():
             #    print(f"{user.firstname} {user.lastname}.....{user.username}")
-        
+        elif option == "Log In":
+            print(" your Username.. ")
+            loginUsername = input()
+            print(" your Pasword ")
+            loginPassword = input()
+            if find_user(loginPassword):
+                print("\n")
+                print("you can create multiple accounts (Create Another) and also view them (View your  Account Details)")
+                print("-" * 30)
+                print("Create Another Account -or- View your Account Details")
+                choose = input()
+                print("\n")
+                if choose == "Create Another Account":
+                    print("Add Your credential of the Account")
+                    print("-" * 15)
+                    accountusername = loginUsername
+                    print("Account Name")
+                    accountname = input()
+                    print("\n")
+                    print("Generate automatic password(Gen) or Create new password(Crt)?")
+                    decision = input()
+                    if decision == "Gen":
+                        characters = string.ascii_letters + string.digits
+                        accountpassword = "".join(choice(characters) for x in range(randint(6, 16)))
+                        print(f"Password: {accountpassword}")
+                    elif decision == "Crt":
+                        print("Enter your Password")
+                        accountpassword = input()
+                    else:
+                        print("Please put in correct details")
+                    save_account(create_account(accountusername, accountname, accountpassword))
+                    print("\n")
+                    print(f"Username:{accountusername} \nAccount Name: {accountname} \nPassword: {accountpassword}")
+
+                elif choose == "View your Account Details":
+                    if find_account(accountusername):
+                        print("Here is a list of your created accounts: ")
+                        print("-" * 15)
+                        for user in display_accounts():
+                            print(f"Account: {user.accountname} \nPassword: {user.accountpassword} \n\n")
+
+                    else:
+                        print("Invalid credentials!")
+                else:
+                    print("kindly try again!")
+                    print("\n")
+            else:
+                print("Incorrect Details kindly try again!")
+                print("\n")
+        else:
+            print("Kindly Choose Valid Account Details")
+            print("\n")
+
+
+if __name__ == '__MAIN__':
+    MAIN()
